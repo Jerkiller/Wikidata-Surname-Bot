2e99c4bb64628ffea3a7c79c5393c490e7de799d
luca.favorido <luca.favorido@beantech.it>
2022-04-09 21:20:02 +0200

    surnames

--- libs/surname-creator.js
+++ libs/surname-creator.js
@@ -15,29 +15,61 @@ module.exports = class Surname {
       const soundex = require('soundex-code')
       const caverphone = require( 'caverphone-phonetics' )
       const colognePhonetic = require( 'cologne-phonetic' ).colognePhonetic;
+      const descriptions = {
+        ast: 'apellíu',
+        bar: 'Schreibnam',
+        br: 'anv-tiegezh',
+        ca: 'cognom',
+        cs: 'příjmení',
+        da: 'efternavn',
+        de: 'Familienname',
+        'de-at': 'Familienname',
+        'de-ch': 'Familienname',
+        en: 'family name',
+        'en-ca': 'family name',
+        'en-gb': 'surname',
+        eo: 'familia nomo',
+        es: 'apellido',
+        et: 'perekonnanimi',
+        eu: 'abizen',
+        fi: 'sukunimi',
+        fit: 'sukunimi',
+        fo: 'ættarnavn',
+        fr: 'nom de famille',
+        ga: 'sloinne',
+        gl: 'apelido',
+        hr: 'prezime',
+        is: 'eftirnafn',
+        it: 'cognome',
+        la: 'nomen gentilicium',
+        lt: 'pavardė',
+        lv: 'uzvārds',
+        mi: 'ingoa whānau',
+        nl: 'achternaam',
+        oc: 'nom d\'ostal',
+        pl: 'nazwisko',
+        pms: 'cognòm',
+        pt: 'sobrenome',
+        'pt-br': 'nome de família',
+        ro: 'nume de familie',
+        sco: 'faimily name',
+        sk: 'priezvisko',
+        sl: 'priimek',
+        sq: 'mbiemër',
+        sv: 'efternamn',
+        tr: 'soyadı',
+        wa: 'no d\'famile',
+        zu: 'isibongo',
+      };
+      const labels = {};
+      Object.keys(descriptions).map(k => {
+        labels[k] = this.surname;
+      });
       const entity = {
         type: 'item',
-        labels: {
-            it: this.surname,
-            en: this.surname,
-            fr: this.surname,
-            de: this.surname,
-            es: this.surname
-        },
-        descriptions: {
-            it: 'cognome',
-            en: 'family name',
-            fr: 'nom de famille',
-            de: 'Familienname',
-            es: 'apellido'
-        },
-        aliases: {
-            it: [],
-            en: [],
-            fr: [],
-            de: [],
-            es: []
-        },
+        labels: labels,
+        descriptions,
+        aliases: {},
         claims: {},
         sitelinks: {}
       };
